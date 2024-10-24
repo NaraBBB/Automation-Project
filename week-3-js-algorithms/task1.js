579db56bbf0e872cad39798a353db920352f983d
NaraBBB <nara.bbb@yahoo.com>
2024-10-24 16:57:14 -0500

    Organize all website files into week-2-html-website folder

---
+++ week-3-js-algorithms/task1.js
@@ -0,0 +1,21 @@
+const first = 5
+const second = 4
+
+// Add your code here                                                              
+if(first > second){
+    console.log(`${first} is bigger than variable second`)
+}else{
+    console.log(`${second} is bigger than variable first`)
+}
+
+//2 - Create a function that returns the length of a string
+function getStringLength(str) {   
+    // Add code here to return the length of str
+return str.length;
+
+  }
+  
+  console.log(getStringLength('sergii')) // 6
+  console.log(getStringLength('alex')) // 4
+
+  //3 - Create a function called divideByTwo. It should accept 1 parameter, divide it  by 2 and return the result of the division
