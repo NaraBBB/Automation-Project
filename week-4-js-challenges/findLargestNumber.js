405daa3f11a87918a1ec8c2458867acbd418e4a5
NaraBBB <nara.bbb@yahoo.com>
2024-11-10 14:58:20 -0600

    Updated findLargestNumber.js

--- week-4-js-challenges/findLargestNumber.js
+++ week-4-js-challenges/findLargestNumber.js
@@ -11,8 +11,6 @@ function findLargestNumber(numbers){
    
         }
     
-    
-
 
 console.log(findLargestNumber([1, 2, 3, 4, 5])); // 5
 console.log(findLargestNumber([10, 20, 30, 40, 50])); // 50
