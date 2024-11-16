329760ad6b59cfe33c0e869ab9d7f3c903b8eb11
NaraBBB <nara.bbb@yahoo.com>
2024-11-16 14:30:14 -0600

    fixed minor issues' clear

--- week-6-JS-Challenge/solutions.js
+++ week-6-JS-Challenge/solutions.js
@@ -2,8 +2,8 @@ import helperMethods from "./helper.methods.js";
 import response from './response.matches.json' assert { type: 'json' };
 
 
-console.log(helperMethods.filterMatchesByLocationAndStatus(response)); // Returns: ["10-016", "10-015"] //[ '10-016', '10-015' ]
-console.log(helperMethods.filterMatchesByCoatingAndShower(response)); // Returns: ["10-015"]
-console.log(helperMethods.filterMatchesByLocationAndPrice(response)); // Returns: ["10-015"]
-console.log(helperMethods.filterMatchesByCreatorFeeAndStatus(response)); // Returns: ["10-016", "10-015"]
-console.log(helperMethods.filterMatchesByCoveringAndDressingRoom(response)); // Returns: ["10-016"]
+console.log(helperMethods.filterMatchesByLocationAndStatus(response)); 
+console.log(helperMethods.filterMatchesByCoatingAndShower(response)); 
+console.log(helperMethods.filterMatchesByLocationAndPrice(response)); 
+console.log(helperMethods.filterMatchesByCreatorFeeAndStatus(response)); 
+console.log(helperMethods.filterMatchesByCoveringAndDressingRoom(response)); 
