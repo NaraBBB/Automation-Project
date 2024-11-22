b462c3e99fdcdd3c670d579073f58e86c0edba73
NaraBBB <nara.bbb@yahoo.com>
2024-11-22 13:09:47 -0600

    separated the Logout as a separate test

--- week-6-cy-automation/cypress/e2e/login/login.positive.cy.js
+++ week-6-cy-automation/cypress/e2e/login/login.positive.cy.js
@@ -1,7 +1,10 @@
-describe("Login Tests", () => {
-  it("Should successfully login and logout with valid credentials", () => {
+describe("Login and Logout Tests", () => {
+  beforeEach(function() {
     cy.viewport(1366, 768);
     cy.visit("/login"); 
+  });
+
+  it("Should successfully login with valid credentials", () => {
     cy.xpath('//a[@href="/auth/login"]').click();
     cy.xpath('//input[@name="email"]').type("waped44366@gianes.com");
     cy.xpath('//input[@type="password"]').type("DontTestMe");
@@ -9,9 +12,17 @@ describe("Login Tests", () => {
 
     cy.get("h6").should("have.text", "Mara  B");
     cy.get("a p").should("have.text", "role: user");
+  });
 
-    cy.xpath("//button[contains(@class,'css-w5qhhs')]").click();
-    cy.xpath("//li[text() = 'Logout']").click()
-  
+  it("Should successfully logout", () => {
+    cy.xpath('//a[@href="/auth/login"]').click();
+    cy.xpath('//input[@name="email"]').type("waped44366@gianes.com");
+    cy.xpath('//input[@type="password"]').type("DontTestMe");
+    cy.get('[type="submit"]').click();
+    cy.get("h6").should("have.text", "Mara  B");
+    cy.get("a p").should("have.text", "role: user");
+    
+    cy.get('button [data-testid="PersonIcon"]').click();
+    cy.xpath("//li[text() = 'Logout']").click();
   });
 });
