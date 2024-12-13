f87c70184681e2c207317e40abf81394a3999151
NaraBBB <nara.bbb@yahoo.com>
2024-12-13 16:30:49 -0600

    Merge week-8-cy-automation folder from tester branch into main

---
+++ week-8-cy-automation/cypress/e2e/login/login.positive.cy.js
@@ -0,0 +1,13 @@
+import loginPage from "../../page_objects/loginPage";
+import dashboardPage from "../../page_objects/dashboardPage";
+describe("User Login Functionality", () => {
+  beforeEach(function () {
+    cy.visit("auth/login");
+    cy.fixture("example").as("credentials");
+  });
+
+  it("should log in successfully with valid credentials", function () {
+    loginPage.loginMethod(this.credentials.email, this.credentials.password);
+    dashboardPage.roleLabel.should("have.text", "role: realtor");
+  });
+});
