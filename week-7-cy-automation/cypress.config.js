052ffe29446a320933e4141218358437117a6978
NaraBBB <nara.bbb@yahoo.com>
2024-12-16 09:59:10 -0600

    modified cypress config.js file

--- week-7-cy-automation/cypress.config.js
+++ week-7-cy-automation/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://dev.delekhomes.com',
-    viewportWidth: 1400,  // Set to desired width, e.g., 1400 pixels
-    viewportHeight: 900,  // Set to desired height, e.g., 900 pixels
+    viewportWidth: 1400, 
+    viewportHeight: 900,  
   },
 })
